c856bc5b0dfdc1276d776fd0c6e6ee5d132a087f
alexmilic <alexmilic01@gmail.com>
2024-08-28 20:34:55 +0200

    Get form data

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -17,9 +17,17 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+    console.log(customerData);
+  }
+
   return (
     <Modal open={userProgressCtx.progress === 'checkout'} onClose={hanldeClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Full Name" type="text" id="full-name" />

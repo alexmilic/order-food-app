572979e9d1290550b79b82781a95ced14e94d898
alexmilic <alexmilic01@gmail.com>
2024-08-29 12:08:50 +0200

    Added post method for checkout submit

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,7 +3,7 @@ import { currencyFormatter } from "../util/formatting"
 export default function CartItem({ name, quantity, price, onIncrease, onDecrease }) {
   return (
     <li className="cart-item">
-      <p>{name} - {quantity} * {currencyFormatter.format(price)}</p>
+      <p>{name} - {quantity} x {currencyFormatter.format(price)}</p>
       <p className="cart-item-actions">
         <button onClick={onDecrease}>-</button>
         <span>{quantity}</span>

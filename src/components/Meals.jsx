7e66cb04979109c3b0d9c8f2352ad87703875045
alexmilic <alexmilic01@gmail.com>
2024-08-31 12:43:32 +0200

    Added new custom hook for fetch httpRequests

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,19 @@
-import { useEffect, useState } from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.jsx";
+
+const requestConfig = {}
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
+  if(isLoading) {
+    return <p>Fetching meals...</p>
+  }
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
-
-      if (!response.ok) {
-
-      }
-
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
-  }, [])
 
   return (
     <ul id="meals">
